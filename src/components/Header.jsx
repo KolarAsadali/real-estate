1448984eaa49129ae53ae8e12e6dc9efd23a86d4
KolarAsadali <kolarasadalis3@gmail.com>
2025-06-28 14:27:24 +0530

    i changed the header image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import {motion} from "framer-motion"
 const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full 
-    overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
-        <Navbar />
+    overflow-hidden' style={{backgroundImage: "url('/real-estate/header_img.png')"}} id='Header'>
+      <Navbar />
         <motion.div
         initial = {{opacity:0, y:100}}
         transition={{duration:1.5}}
